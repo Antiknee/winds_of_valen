d7d04fe4ac474e3d9e730c425a0fcf09db1e25eb
Antiknee <59623183+Antiknee@users.noreply.github.com>
2026-09-06 00:08:59 +0100

    Restructure project into clean winds_of_valen package layout

--- smithing/functions/is_smelting_item.py
+++
@@ -1,5 +0,0 @@
-from smithing.dictionaries.recipes_material import material_breakdown_recipes
-from smithing.dictionaries.recipes_item import item_recipes
-
-def is_smelting_item(name: str) -> bool:
-    return name in material_breakdown_recipes or name in item_recipes
